52f8f60d51676cee4b16af0f355aefe415355776
ynack <nakayama.yosuke@gmail.com>
2024-03-07 18:08:04 +0900

    デザイン修正

--- testapp/models/device.py
+++ testapp/models/device.py
@@ -7,6 +7,6 @@ class Device(db.Model):
     name = db.Column(db.String(255)) #デバイスの名前
     type = db.Column(db.String(255)) #デバイスの種類
     model = db.Column(db.String(255)) #モデル番号
-    year = db.Column(db.Date) #発売開始年月日
+    year = db.Column(db.Date) #販売開始年月日
     create_at = db.Column(db.DateTime, nullable=False, default=datetime.now)    #登録日時
     update_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now) #更新日時
--- testapp/views.py
+++ testapp/views.py
@@ -8,17 +8,59 @@ import datetime
 def main():
     return render_template('testapp/index.html')
 
+@app.route('/devices')
+def device_list():
+    devices = Device.query.all()
+    return render_template('testapp/device_list.html', devices=devices)
+
 @app.route('/add_device', methods=['GET', 'POST'])
-def add_route():
+def add_device():
     if request.method == 'GET':
         return render_template('testapp/add_device.html')
     if request.method == 'POST':
-        device = Device(
-            name = 'iMac',
-            type = 'Mac',
-            model = 'MQRxxJ/A',
-            year = datetime.date(2023,10,30)
-        )
-        db.session.add(device)
-        db.session.commit()
-        return redirect(url_for('main'))
+            form_name = request.form.get('name')
+            form_type = request.form.get('type')
+            form_model = request.form.get('model')
+            form_year = request.form.get('year').split('-') #受け取ったform_yearを'-'で分割して配列に
+            list_year = [int(i) for i in form_year] #配列にしたform_yearの格値をinteger型に変換。datetime.dateに入れる値がinteger型の必要があるため
+
+            device = Device(
+                name = form_name,
+                type = form_type,
+                model = form_model,
+                year = datetime.date(list_year[0],list_year[1],list_year[2])
+            )
+            db.session.add(device)
+            db.session.commit()
+            return redirect(url_for('device_list'))
+
+@app.route('/devices/<int:id>/')
+def device_detail(id):
+    device = Device.query.get(id)
+    return render_template('testapp/device_detail.html', device=device)
+
+@app.route('/devices/<int:id>/edit', methods=['GET'])
+def device_edit(id):
+    device = Device.query.get(id)
+    return render_template('testapp/device_edit.html', device=device)
+
+@app.route('/devices/<int:id>/update', methods=['POST'])
+def device_update(id):
+    device = Device.query.get(id)
+    device.name = request.form.get('name')
+    device.type = request.form.get('type')
+    device.model = request.form.get('model')
+    form_year = request.form.get('year').split('-') #受け取ったform_yearを'-'で分割して配列に
+    list_year = [int(i) for i in form_year]
+    device.year = datetime.date(list_year[0],list_year[1],list_year[2])
+
+    db.session.merge(device)
+    db.session.commit()
+    return redirect(url_for('device_list'))
+
+@app.route('/devices/<int:id>/delete', methods=['GET', 'POST'])
+def device_delete(id):
+    device = Device.query.get(id)
+    db.session.delete(device)
+    db.session.commit()
+    return redirect(url_for('device_list'))
